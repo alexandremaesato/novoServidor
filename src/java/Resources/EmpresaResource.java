1eb7890b010dc2b95bacace18401513b5d08c24d
alexandremaesato <alexandremaesato@gmail.com>
2016-04-14 20:17:35 -0300

    Cadastro comecando a funcionar

--- src/java/Resources/EmpresaResource.java
+++ src/java/Resources/EmpresaResource.java
@@ -5,13 +5,24 @@
  */
 package Resources;
 
+
+
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -19,6 +30,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.xml.bind.DatatypeConverter;
 import model.AutenticacaoDao;
 import model.Empresa;
 import model.EmpresaDAO;
@@ -59,41 +73,45 @@ public class EmpresaResource {
         return emps;
     }
 
-    @POST
+    @POST   
     @Path("/cadastrarEmpresa")
-    @Consumes("application/json")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces("application/json")
     public String cadastrarEmpresa(String val) throws SQLException {
+        Gson gson = new Gson(); 
+        Empresa emp = new Empresa();
+        String coded = null;
+        Map<String, String> map = new LinkedHashMap<String, String>();
+        for(String keyValue : val.split(" *, *")) {
+            String[] pairs = keyValue.split(" *= *", 2);
+            coded = pairs[1];
+            map.put(pairs[0], pairs.length == 1 ? "" : pairs[1]);
+        }
         
-//        HashMap params = gson.fromJson(val, HashMap.class );
-//        String jsonEmpresa = (String) params.get("empresa").toString();
-//        String jsonPessoa = (String) params.get("empresa").toString();
-//       
-//        //Precisa criar verificacao de todos os dados para nao dar erro no DAO
-//        Empresa objetoEmpresa = gson.fromJson(jsonEmpresa, Empresa.class);
-//        Pessoa objetoPessoa   = gson.fromJson(jsonPessoa, Pessoa.class);
-//        int idEntidade = empresadao.cadastrarEmpresa(objetoEmpresa, objetoPessoa);
-//        
-//        Imagem img = objetoEmpresa.getImagemPerfil();
-//        byte[] imagem = img.getImg().getBytes(Charset.forName("UTF-8"));
-//        String img_name = "img-" + System.currentTimeMillis() + img.getNomeImagem();
-//        try{
-//            FileOutputStream fos = new FileOutputStream("imagensPerfil/" + img_name );
-//            fos.write(imagem);
-//            FileDescriptor fd = fos.getFD();
-//            fos.flush();
-//            fd.sync();
-//            fos.close(); 
-//        }
-//        catch(Exception e){
-//           throw new RuntimeException("Erro ao gravar imagem. " + e);
-//        }
-//        
-//        img.setNomeImagem(img_name);
-//        img.setCaminho("imagensPerfil/" + img_name);
+        byte[] encodedHelloBytes = DatatypeConverter.parseBase64Binary(coded);
+        String decodeString = new String(encodedHelloBytes, StandardCharsets.UTF_8);
+        emp = gson.fromJson(decodeString, Empresa.class);
+       
+        Imagem img = emp.getImagemPerfil();
+        byte[] imagem = img.getImg().getBytes(Charset.forName("UTF-8"));
+        String img_name = "img-" + System.currentTimeMillis() + img.getNomeImagem();
+        try{
+            FileOutputStream fos = new FileOutputStream("imagensPerfil/" + img_name );
+            fos.write(imagem);
+            FileDescriptor fd = fos.getFD();
+            fos.flush();
+            fd.sync();
+            fos.close(); 
+        }
+        catch(Exception e){
+           throw new RuntimeException("Erro ao gravar imagem. " + e);
+        }
+        
+        img.setNomeImagem(img_name);
+        img.setCaminho("imagensPerfil/" + img_name);
 //        img.setPessoaid(autdao.getPessoaId(objetoPessoa));
 //        img.setItemid(idEntidade);
-//        imgdao.inserirIMagem(img);
+        imgdao.inserirIMagem(img);
         
         return gson.toJson("Cadastrado com Sucesso!");
     }
